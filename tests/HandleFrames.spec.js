57377b3f98c708dcf5a92ced7d2c3b88662c3665
Mehmet-Altuntas1986 <mehmetscience001@gmail.com>
2024-03-25 06:46:30 +0100

    frames and iframes

--- tests/HandleFrames.spec.js
+++ tests/HandleFrames.spec.js
@@ -10,11 +10,11 @@ test('frames', async ({ page }) => {
 
   //approach 1: using  name or url
   //const var=await page.frame('name'); // if name is present 
-  //const frame1=await page.frame({url:'https://ui.vision/demo/webtest/frames/frame_1.html'})
+  //const frame1=await page.frame({url:'https://ui.vision/demo/webtest/frames/frame_1.html'})  //<frame noresize src="frame 1.html"> url boyle gozukuyordu , saga tiklayip gercek url i buldu
   //await frame1.fill("[name='mytext1']",'Hello');
 
 
-  //appraoch 2- using frame locator
+  //appraoch 2- using frame locator -- more populer way
   const inputbox=await page.frameLocator("frame[src='frame_1.html']").locator("[name='mytext1']")
   inputbox.fill("Hello")
 
